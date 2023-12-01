42bae5222b6d1aa36b651f051f2bb68e08e9b33a
amaan <amaanx20@hotmail.co.uk>
2023-12-01 15:00:57 +0000

    Commit 1

---
+++ week2.py
@@ -0,0 +1,10 @@
+name = input("Whats your name?")
+print("Hello there,", name, "Good to meet you.")
+
+dog = float(input("Enter the temp in Celsius:"))
+fahrenheit = (dog * 1.8 + 32)
+print(dog, "is equivalent to", fahrenheit, "In Fahrenheit")
+
+students = int(input("How many students are there?"))
+groups = int(input("What's your group size?"))
+rat = (students / 24)
