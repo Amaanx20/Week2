7673dada993792bc613bcbafed9ffb7aa34c4902
amaan <amaanx20@hotmail.co.uk>
2023-12-04 22:58:27 +0000

    Commit 3

--- week2.py
+++ week2.py
@@ -7,4 +7,13 @@ print(dog, " Celsius is equivalent to", fahrenheit, "In Fahrenheit")
 
 students = int(input("How many students are there?"))
 groups = int(input("What's your group size?"))
-rat = (students / 24)
+fox = (students//groups)
+rat = fox
+bat = (students % groups)
+print("There will be", fox, "groups with", bat, "Students left over.")
+
+eagle = int(input("How many sweets?"))
+beagle = int(input("How many kids?"))
+regal = (eagle//beagle)
+racoon = (eagle % beagle)
+print("there are", beagle, "kids and each kid gets", regal, "Sweets, There are", racoon, "Sweets left over")
